b70342a30e602de1415c9f86e3dc4a9d8c6ab06d
fanqieEgg <994640299@qq.com>
2020-10-27 10:14:49 +0800

    delete

--- testGitHub2/源2.cpp
+++ testGitHub2/源2.cpp
@@ -3,13 +3,6 @@ using namespace std;
 
 int main()
 {
-	int a = 0;
-	int b = 2;
-	cout << a - b << endl;
-	cout << "test Github" << endl;
-	cout << "test Github" << endl;
-	cout << "test Github" << endl;
-	cout << "test Github" << endl;
-	cout << "test Github" << endl;
+	
 	return 0;
 }
